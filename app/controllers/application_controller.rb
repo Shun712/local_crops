6dca71195ac5b5afd92dd8666164d8e271556b38
Shun712 <sh705vi@gmail.com>
2022-04-07 21:05:54 +0900

    [Modify]Userのストロングパラメーター設定をユーザー名を保存できるよう修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # フラッシュメッセージのキーを許可する
   add_flash_types :success, :info, :warning, :danger
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # user登録でユーザー名を登録できるようにする
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
