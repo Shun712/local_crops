6a48f334211bd838837106098a43a2a1aab258f3
Shun712 <sh705vi@gmail.com>
2022-04-07 18:38:52 +0900

    [Add]generators時に不要なファイルを生成しないように設定

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,15 @@ module LocalCrops
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
+    config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
+    
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
