7eea7a5bd2560eaadea1a45d09955d5708418eac
Shun712 <sh705vi@gmail.com>
2022-04-19 02:21:55 +0900

    [Add]User登録のシステムスペックを追加

--- spec/system/users/users_spec.rb
+++ spec/system/users/users_spec.rb
@@ -11,7 +11,19 @@ RSpec.describe 'ユーザー登録' do
       click_button 'アカウント登録'
       expect(page).to have_content '本人確認用のメールを送信しました。メール内のリンクからアカウントを有効化させてください'
       visit current_path
+      expect(page).not_to have_content '本人確認用のメールを送信しました。メール内のリンクからアカウントを有効化させてください'
+    end
+  end
 
+  context '入力情報に間違いがある場合' do
+    it '新規登録できないこと' do
+      visit new_user_registration_path
+      fill_in 'ユーザー名', with: ' '
+      fill_in 'メールアドレス', with: 'test@example.com'
+      fill_in 'パスワード(6文字以上)', with: 'password'
+      fill_in 'パスワード確認', with: 'password'
+      click_button 'アカウント登録'
+      expect(page).to have_content 'ユーザー名を入力してください'
     end
   end
 end
