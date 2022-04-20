5793e54c77b6773109746c25857560ca058dc33f
Shun712 <sh705vi@gmail.com>
2022-04-21 01:10:37 +0900

    [Add]静的ページのコントローラとビューを生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # Deviseのマッピングはするが、skipして何も設定しない
   devise_for :users, skip: %i[sessions registrations passwords],
-                     controllers: {
-                       confirmations: 'users/confirmations',
-                       omniauth_callbacks: 'users/omniauth_callbacks'
-                     }
+             controllers: {
+               confirmations: 'users/confirmations',
+               omniauth_callbacks: 'users/omniauth_callbacks'
+             }
   devise_scope :user do
     get 'login', to: 'users/sessions#new', as: :new_user_session
     post 'login', to: 'users/sessions#create', as: :user_session
@@ -16,8 +16,8 @@ Rails.application.routes.draw do
     get 'password/edit', to: 'users/passwords#edit', as: :edit_user_password
   end
   root 'home#index'
-  # get 'about', to: 'static_pages/about'
-  # get 'use_of_terms', to: 'static_pages/terms'
-  # get 'feedbacks', to: 'static_pages/feedbacks'
-  # get 'privacy_policy', to: 'static_pages/privacy_policy'
+  get 'about', to: 'static_pages#about'
+  get 'use_of_terms', to: 'static_pages#terms'
+  get 'feedbacks', to: 'static_pages#feedbacks'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
 end
