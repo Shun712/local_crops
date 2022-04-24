d7c8ebd1b707f1ae13200411d0933dc91ab772cf
Shun712 <sh705vi@gmail.com>
2022-04-24 23:39:50 +0900

    [Add]フィードバックモデルを作成

---
+++ app/models/feedback.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: feedbacks
+#
+#  id         :bigint           not null, primary key
+#  body       :text(65535)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_feedbacks_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+class Feedback < ApplicationRecord
+  belongs_to :user
+  validates :body, presence: true, length: { maximum: 1000 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_13_050953) do
+ActiveRecord::Schema.define(version: 2022_04_24_143342) do
+
+  create_table "feedbacks", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_feedbacks_on_user_id"
+  end
 
   create_table "social_profiles", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -40,5 +48,6 @@ ActiveRecord::Schema.define(version: 2022_04_13_050953) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "feedbacks", "users"
   add_foreign_key "social_profiles", "users"
 end
