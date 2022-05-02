49883ad696492dd79b666adbdfeebaa11907eec0
Shun712 <sh705vi@gmail.com>
2022-05-03 00:21:37 +0900

    [Add]バリデーションを設定

--- app/models/crop.rb
+++ app/models/crop.rb
@@ -24,4 +24,24 @@ class Crop < ApplicationRecord
   validates :description, length: { maximum: 1000 }
   validates :harvested_on, presence: true
   has_one_attached :picture
+  validate :file_type
+  validate :file_size
+
+  private
+
+  def file_type
+    pictures.each do |picture|
+      if !picture.blob.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:pictures, 'は JPEG 形式または PNG 形式のみ選択してください')
+      end
+    end
+  end
+
+  def file_size
+    pictures.each do |picture|
+      if picture.blob.byte_size > 5.megabytes
+        errors.add(:pictures, 'は 5MB 以下のファイルを選択してください')
+      end
+    end
+  end
 end
