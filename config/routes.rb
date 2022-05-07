a38de52535ac7d67b018ddbceebada6d47fcb3f3
Shun712 <sh705vi@gmail.com>
2022-05-08 07:44:41 +0900

    [Add]プロフィール編集用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,9 @@ Rails.application.routes.draw do
   resources :feedbacks, only: %i[new create]
   resources :crops
   resources :users, only: %i[show]
+  namespace :mypage do
+    resources :account, only: %i[edit update]
+  end
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: '/letter_opener'
   end
