692aac98a417df14e0520d40517353507e3c376d
Shun712 <sh705vi@gmail.com>
2022-05-15 08:44:22 +0900

    [Modify]seedファイルの修正

--- db/seeds/crops.rb
+++ db/seeds/crops.rb
@@ -4,7 +4,7 @@ User.limit(3).each do |user|
                         description: Faker::Hacker.say_something_smart,
                         harvested_on: Faker::Date.between(from: 3.days.ago, to: Date.today)
   )
-  crop.picture.attach(io: File.open(Rails.root.join('app/assets/images/400x200-crop-dummy.png')), filename: '400x200-crop-dummy.png.png')
+  crop.picture.attach(io: File.open(Rails.root.join('app/assets/images/400x200-crop-dummy.png')), filename: '400x200-crop-dummy.png')
   crop.save!
   puts "crop#{crop.id} has created!"
 end
