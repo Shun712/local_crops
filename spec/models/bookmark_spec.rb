71c1bdb8bcdda973aae1e8d64c5a3c348e2abe6d
Shun712 <sh705vi@gmail.com>
2022-05-16 02:38:34 +0900

    [Modify]be rails db:migrate

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: bookmarks
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  crop_id    :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_bookmarks_on_crop_id              (crop_id)
+#  index_bookmarks_on_user_id              (user_id)
+#  index_bookmarks_on_user_id_and_crop_id  (user_id,crop_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (crop_id => crops.id)
+#  fk_rails_...  (user_id => users.id)
+#
 require 'rails_helper'
 
 RSpec.describe Bookmark, type: :model do
