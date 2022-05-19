3ee317850d09ed5ea1e2517480b75ca1bfbfb086
Shun712 <sh705vi@gmail.com>
2022-05-20 06:08:56 +0900

    [Add]chatsコントローラの作成

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,24 @@
+class ChatsController < ApplicationController
+  def create
+    @chat = current_user.chats.create(chat_params)
+  end
+
+  def edit
+    @chat = current_user.chats.find(params[:id])
+  end
+
+  def update
+    @chat = current_user.chats.find(params[:id])
+    @chat.update!(chat_update_params)
+  end
+
+  def destroy
+    @chat = current_user.chats.find(params[:id])
+    @chat.destroy!
+  end
+
+  private
+  def chat_params
+    params.require(:chat).permit(:body)
+  end
+end
