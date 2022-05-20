334d07fc5f9635d499cb12549bc09a67bc29d06c
Shun712 <sh705vi@gmail.com>
2022-05-21 06:24:16 +0900

    [Modify]使用しないアクションを削除

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -7,13 +7,6 @@ class ChatroomsController < ApplicationController
                              .per(12)
   end
 
-  def create
-    user = User.find(params[:id])
-    @chatroom = Chatroom.chatroom_for_partner(user)
-    @chats = @chatroom.chats.sorted.limit(100).reverse
-    redirect_to chatroom_path(@chatroom)
-  end
-
   def show
     @chatroom = current_user.chatrooms
                             .includes(:user)
