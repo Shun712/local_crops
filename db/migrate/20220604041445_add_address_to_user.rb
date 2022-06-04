af5fd12d4c7c8417c663910704ce94c11bc41635
Shun712 <sh705vi@gmail.com>
2022-06-04 16:53:39 +0900

    [Modify]タイポ修正

--- db/migrate/20220604041445_add_address_to_user.rb
+++ db/migrate/20220604041445_add_address_to_user.rb
@@ -1,4 +1,4 @@
-class AddAdressToUser < ActiveRecord::Migration[6.1]
+class AddAddressToUser < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :address, :string
     add_column :users, :latitude, :float
