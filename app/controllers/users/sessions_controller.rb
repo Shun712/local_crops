6b0b68e7066992744ade04cb3a4f1de71aabe664
Shun712 <sh705vi@gmail.com>
2022-06-19 16:42:02 +0900

    [Modify]本番環境用にdatabase.ymlとdocker-compose.ymlを修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,12 +4,21 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def create
-    super
+    # super
+    devise_create
   end
 
   def destroy
     # super
     devise_destroy
+  end
+
+  def devise_create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:success, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
   end
 
   def devise_destroy
