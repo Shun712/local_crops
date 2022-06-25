3bdba284b086f39daef94202d44944cca3d339bd
Shun712 <sh705vi@gmail.com>
2022-06-25 23:23:01 +0900

    [Modify]ソートのロジックを修正

--- app/controllers/crops_controller.rb
+++ app/controllers/crops_controller.rb
@@ -1,17 +1,22 @@
 class CropsController < ApplicationController
   def index
     if params[:q]
-      @crops = @q.result(distinct: true)
-                 .includes(:user)
-                 .page(params[:page])
-                 .per(12)
+      sorted_crops = @q.result(distinct: true)
+                       .harvested_within_a_week
+                       .not_reserved
+                       .select do |crop|
+        current_user.distance_within_5km?(crop)
+      end
+      @crops = Crop.where(id: sorted_crops.map { |crop| crop.id })
+                   .includes(:user)
+                   .page(params[:page])
+                   .per(12)
     else
       # 収穫1週間以内、未予約、距離5km以内の作物を取得
-      local_crops = []
-      Crop.harvested_within_a_week.not_reserved.each do |crop|
-        local_crops << crop if current_user.distance_within_5km?(crop)
+      local_crops = Crop.harvested_within_a_week.not_reserved.select do |crop|
+        current_user.distance_within_5km?(crop)
       end
-      @crops = Crop.where(id: local_crops.map{ |crop| crop.id })
+      @crops = Crop.where(id: local_crops.map { |crop| crop.id })
                    .includes(:user)
                    .sorted
                    .page(params[:page])
@@ -58,10 +63,16 @@ class CropsController < ApplicationController
   end
 
   def search
-    @crops = @q.result(distinct: true)
-               .includes(:user)
-               .page(params[:page])
-               .per(12)
+    searched_crops = @q.result(distinct: true)
+                       .harvested_within_a_week
+                       .not_reserved
+                       .select do |crop|
+      current_user.distance_within_5km?(crop)
+    end
+    @crops = Crop.where(id: searched_crops.map { |crop| crop.id })
+                 .includes(:user)
+                 .page(params[:page])
+                 .per(12)
   end
 
   private
